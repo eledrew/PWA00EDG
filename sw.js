701e4ebf6be1f81db3285828f03fa8bfc0e74ba6
eledrew <73409094+eledrew@users.noreply.github.com>
2021-03-16 13:49:44 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.addEventListener('activate',event => {
 });
 
 // The fetch handler
-self.addEventListener('fetch', function(event){
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
